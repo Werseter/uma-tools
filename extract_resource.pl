39538e6716fef8817a72c996c82a433e54171f35
Werseter <werseter@interia.eu>
2025-11-20 20:37:10 +0100

    Ensure need_unpack directory exists during resource extraction

--- extract_resource.pl
+++ extract_resource.pl
@@ -5,6 +5,7 @@ use utf8;
 
 use Cwd 'abs_path';
 use File::Basename;
+use File::Path qw(make_path);
 use DBI;
 use DBD::SQLite::Constants qw(:file_open);
 
@@ -42,6 +43,11 @@ say "SELECT h, e FROM a WHERE n LIKE \"$query\";";
 my $select = $metadb->prepare("SELECT h, e FROM a WHERE n LIKE \"$query\";");
 
 my ($hash, $keyint);
+
+# make sure need_unpack directory exists
+unless (-d 'need_unpack') {
+	make_path('need_unpack') or die "Failed to create need_unpack: $!";
+}
 
 $select->execute;
 $select->bind_columns(\($hash, $keyint));
